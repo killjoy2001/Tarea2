439af828071d11d7cb4ad587d46fb488ed04092e
kenny <kenny.ara.cas.ka@gmail.com>
2022-09-29 11:28:30 -0600

    Tarea 2

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tarea_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] empleado = { "Kenny", "Maria", "Pablo", "Ana", "Mauricio" };
+            int[] salario = { 800, 1200, 600, 1500, 1000 };
+
+            for (int i = 0; i < salario.Length; i++)
+            {
+                salario[i] *= 12;
+                Console.WriteLine("El aguinaldo de " + empleado[i] + " equivale a " + salario[i] );
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
